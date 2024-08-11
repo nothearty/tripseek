2d0c346e6583ad13abd13bd161156b8e178f87f4
xenopers <105857376+xenopers@users.noreply.github.com>
2024-08-11 10:46:20 +0200

    Added text to landing page

--- frontend/src/components/GetStarted.tsx
+++ frontend/src/components/GetStarted.tsx
@@ -4,12 +4,13 @@ export default function GetStarted() {
   return (
     <div className='container mx-auto text-center p-6 bg-white min-h-screen flex flex-col relative items-center justify-center'>
       <h1 className='max-w-5xl mx-auto text-4xl sm:text-6xl font-bold mb-4 sm:mb-6 flex justify-center flex-wrap leading-tight text-gray-900'>
-        Lorem Ipsum Dolor Sit{" "}
-        <span className='text-lime-500'>Lorem Ipsum Dolor</span>
+        Effortless Travel," "}
+        <span className='text-lime-500'>Expertly Planned{</span>
       </h1>
       <p className='max-w-5xl mx-auto sm:leading-2 text-base sm:text-xl mb-4 sm:mb-6  text-gray-600 sm:px-36'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugiat
-        beatae magnam recusandae accusantium alias totam sit, quidem
+        Transform your travel dreams into reality. 
+        Our AI-driven planner curates detailed itineraries based on your preferences, 
+        ensuring every trip is perfectly suited to you.
       </p>
       <a
         href='/trip-form'
