a22e46afeb1b8f591ea22f64c50e6f1719e2dfa6
nothearty <53410727+nothearty@users.noreply.github.com>
2024-08-12 13:29:10 +0200

    fixed GetStarted.tsx

--- frontend/src/components/GetStarted.tsx
+++ frontend/src/components/GetStarted.tsx
@@ -1,16 +1,15 @@
-import travelerImage from "../assets/five.svg"
+import travelerImage from "../assets/five.svg";
 
 export default function GetStarted() {
   return (
     <div className='container mx-auto text-center p-6 bg-white min-h-screen flex flex-col relative items-center justify-center'>
       <h1 className='max-w-5xl mx-auto text-4xl sm:text-6xl font-bold mb-4 sm:mb-6 flex justify-center flex-wrap leading-tight text-gray-900'>
-        Effortless Travel," "}
-        <span className='text-lime-500'>Expertly Planned{</span>
+        Lorem Ipsum Dolor Sit{" "}
+        <span className='text-lime-500'>Lorem Ipsum Dolor</span>
       </h1>
       <p className='max-w-5xl mx-auto sm:leading-2 text-base sm:text-xl mb-4 sm:mb-6  text-gray-600 sm:px-36'>
-        Transform your travel dreams into reality. 
-        Our AI-driven planner curates detailed itineraries based on your preferences, 
-        ensuring every trip is perfectly suited to you.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugiat
+        beatae magnam recusandae accusantium alias totam sit, quidem
       </p>
       <a
         href='/trip-form'
@@ -23,5 +22,5 @@ export default function GetStarted() {
         <img src={travelerImage} className='w-full h-full' />
       </div>
     </div>
-  )
+  );
 }
